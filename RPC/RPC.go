205fddd1443888e1a549fc00c9f3cfde6021de2a
16105091 <16105091@facin.portoalegre.pucrsnet.br>
2019-11-07 12:24:43 -0200

    Improved RPCs

--- RPC/RPC.go
+++ RPC/RPC.go
@@ -8,14 +8,14 @@ import . "../Timer"
 
 // Log Entry (appended $VALUE at $TERM)
 type LogEntry struct {
-	Term 		int
-	Value 		int
+	term 		int
+	value 		int
 }
 
 // Processes' state
 type RecvrInfo struct {
 	// Persistent
-	currentTerm  int
+	currentterm  int
 	votedFor     int
 	log			 make([] LogEntry)
 
@@ -40,7 +40,7 @@ type RecvrInfo struct {
 
 /*
 	State init::
-	currentTerm <- 0
+	currentterm <- 0
 	votedFor    <- nil
 	commitIndex <- 0
 	lastApplied <- 0
@@ -51,99 +51,104 @@ type RecvrInfo struct {
 
 
 // AppendEntry parameters
-type AppendInfo struct {
+type AppendEntriesArgs struct {
 	term         int
 	leaderId     int
 	prevLogIndex int
-	prevLogTerm  int
+	prevLogterm  int
 	entries		 make([] LogEntry)
 	leaderCommit int
 }
 
 // RequestVote parameters
-type CandidateInfo struct {
+type RequestVoteArgs struct {
 	term         int
 	candidateId  int
 	lastLogIndex int
-	lastLogTerm  int
+	lastLogterm  int
 }
 
 // RPCs response
 type ReplyInfo struct {
 	term 		 int
-	reply        int
+	reply        bool
 }
 
 
 // RPC RequestVote (Args, Reply)
-func (state *RecvrInfo) RequestVote(candidate *CandidateInfo, vote *ReplyInfo) error{
+func (state *RecvrInfo) RequestVote(candidate *RequestVoteArgs, vote *ReplyInfo) error{
+	// add semaphore
 	if state.votedFor == nil {  // 2. If votedFor is null or candidateId, grant vote
-		vote.term  = state.currentTerm+1
-		vote.reply = 1
-
-		state.currentTerm  = candidate.term
 		state.votedFor     = candidate.candidateId
+		state.currentterm  = candidate.term
+
+		vote.term  = state.currentterm+1
+		vote.reply = true
 		return nil
 	}
 
-	if state.currentTerm >= candidate.term {  // 1. Reply false if term < currentTerm
-		vote.term  = state.currentTerm+1
-		vote.reply = 0
+	if candidate.term < state.currentterm {  // 1. Reply false if term < currentterm
+		vote.term  = state.currentterm+1
+		vote.reply = false
 		return nil
 	}
 
 	if len(state.log) <= candidate.lastLogIndex {  // 2. If candidate's log is at least as up-to-date as receiver's log, grant vote
-		vote.term  = state.currentTerm+1
-		vote.reply = 1
+		vote.term  = state.currentterm+1
+		vote.reply = true
 
-		state.currentTerm  = candidate.term
+		state.currentterm  = candidate.term
 		state.votedFor     = candidate.candidateId
 		return nil
 	}
+	// end semaphore
 
-	vote.term  = state.currentTerm+1
-	vote.reply = 0
+	vote.term  = state.currentterm+1
+	vote.reply = false
 	return nil
 }
 
 // RPC AppendEntry (Args, Reply)
-func (state *RecvrInfo) AppendEntry(entry *AppendInfo, rep *Reply) error{
-	if entry.term < state.currentTerm {  // 1.  Reply false if term < currentTerm
-		rep.term  = state.currentTerm
-		rep.reply = 0
-		return nil
-	}
-
+func (state *RecvrInfo) AppendEntry(args *AppendEntriesArgs, rep *Reply) error{
 	if !state.timer.Stop(){
 		return nil
 	}
 
-	if len(state.log) < entry.prevLogIndex || // 2.  Reply false if log doesn’t contain an entry at prevLogIndex
-		state.log[entry.prevLogIndex].Term != entry.prevLogTerm { // whose term matches prevLogTerm
-		rep.term  = state.currentTerm
-		rep.reply = 0
-		return nil
+	rep.term  = state.currentterm
+	rep.reply = false
+
+	if args.term < state.currentterm {  // 1.  Reply false if term < currentterm
+		return nil // restart timer
 	}
 
-	if state.log[entry.prevLogIndex+1].Term != entry.prevLogTerm { // 3. If an existing entry conflicts with a new one (same index
-														// but different terms), delete the existing entry and all that follow it
-		// TODO
+	if len(state.log) < args.prevLogIndex || // 2.  Reply false if log doesn’t contain an entry at prevLogIndex
+			state.log[args.prevLogIndex].term != args.prevLogterm { // whose term matches prevLogterm
+		return nil // restart timer
 	}
+
+	// if state.log[args.prevLogIndex].term == args.prevLogterm { // 3. If an existing entry conflicts with a new one (same index
+	// 													// but different terms)
+		state.log = state.log[:args.prevLogIndex+1] // delete the existing entry and all that follow it
+	// }
 
 	// 4. Append any new entries not already in the log
-	// TODO
+	for _, entry := range args.entries {
+		state.log = append(state.log, entry)
+	}
 
-	if entry.leaderCommit > state.commitIndex {
+	if args.leaderCommit > state.commitIndex {
 		// 5. If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
-		if entry.leaderCommit > len(state.log){
+		if args.leaderCommit > len(state.log){
 			state.commitIndex = len(state.log)
 		}
 		else{
-			state.commitIndex = entry.leaderCommit
+			state.commitIndex = args.leaderCommit
 		}
 	}
 
 	remaining := <- state.timer.C
 	remaining.Add(GenRandom())
 	state.timer.Reset(remaining)
+
+	rep.reply = true
 }
